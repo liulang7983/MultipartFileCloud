870a8fcc635b867f82e89c9ecb6a99b1d5d8733f
ming.li <ming.li@goupwith.com>
2023-11-13 13:59:54 +0800

    @PropertySource特殊配置

---
+++ mavenServing/src/main/java/com/maven/lm/MavenSpring.java
@@ -0,0 +1,15 @@
+package com.maven.lm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ming.li
+ * @date 2023/11/13 11:52
+ */
+@SpringBootApplication
+public class MavenSpring {
+    public static void main(String[] args) {
+        SpringApplication.run(MavenSpring.class,args);
+    }
+}
