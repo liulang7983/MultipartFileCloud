9ea99c6114e4ea4f53e4fbde25e566ca271e0eaf
ming.li <1430793456@qq.com>
2024-02-04 11:45:54 +0800

    MultipartFile对象保存为文件

--- producerCloud/src/main/java/com/controller/FasController.java
+++ producerCloud/src/main/java/com/controller/FasController.java
@@ -4,6 +4,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * @author 李明
  * @date Created in 2023/8/31 23:58
@@ -15,8 +18,17 @@ public class FasController {
     String asyncUploadFiles(@RequestPart("files") MultipartFile[] files, @RequestParam("fsUuid")String fsUuid){
         System.out.println(fsUuid);
         System.out.println(files.length);
+        String path="C:\\Users\\14307\\Desktop\\日志\\MultipartFileCloud\\";
         for (int i = 0; i < files.length; i++) {
-            System.out.println(files[i].getOriginalFilename());
+            try {
+                MultipartFile file = files[i];
+                String filename = file.getOriginalFilename();
+                String filePath=path+filename;
+                file.transferTo(new File(filePath));
+                System.out.println(filename);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return "成功";
     }
